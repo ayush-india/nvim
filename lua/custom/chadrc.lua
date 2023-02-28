c61c2aee3dbc8d7cb1841705b5f812f9c21b1e2a
ayushmishra <mail81mitthu@gmail.com>
2023-02-28 21:36:21 +0530

    themeing

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -4,6 +4,24 @@ M.ui = {
   theme = "tokyodark",
   transparency = false,
   theme_toggle = { "tokyodark", "everforest" },
+
+  -- cmp themeing
+  cmp = {
+    icons = false,
+    lspkind_text = false,
+    style = "flat_dark", -- default/flat_light/flat_dark/atom/atom_colored
+    border_color = "grey_fg", -- only applicable for "default" style, use color names from base30 variables
+    selected_item_bg = "simple", -- colored / simple
+  },
+
+  ------------------------------- nvchad_ui modules -----------------------------
+  statusline = {
+    theme = "minimal", -- default/vscode/vscode_colored/minimal
+    -- default/round/block/arrow separators work only for default statusline theme
+    -- round and block will work for minimal theme only
+    separator_style = "round",
+    overriden_modules = nil,
+  },
 }
 
 M.plugins = require "custom.plugins"
