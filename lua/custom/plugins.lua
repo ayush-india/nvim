21efb922e2dbf858185504c0404fb31249d4bc48
ayushmishra <mail81mitthu@gmail.com>
2023-03-08 11:18:48 +0530

    added lazy.nvim

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,65 @@
+local overrides = require("custom.configs.overrides")
+
+---@type NvPluginSpec[]
+local plugins = {
+
+  -- Override plugin definition options
+
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      -- format & linting
+      {
+        "jose-elias-alvarez/null-ls.nvim",
+        config = function()
+          require("custom.configs.null-ls")
+        end,
+      },
+      {
+        "williamboman/mason.nvim",
+        config = function(_, opts)
+          dofile(vim.g.base46_cache .. "mason")
+          require("mason").setup(opts)
+          vim.api.nvim_create_user_command("MasonInstallAll", function()
+            vim.cmd("MasonInstall " .. table.concat(opts.ensure_installed, " "))
+          end, {})
+          require("custom.configs.lspconfig") -- Load in lsp config
+        end,
+      },
+      "williamboman/mason-lspconfig.nvim"
+    },
+    config = function() end, -- Override to setup mason-lspconfig
+  },
+
+  -- overrde plugin configs
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = overrides.treesitter,
+  },
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = overrides.nvimtree,
+  },
+
+  -- Install a plugin
+  {
+    "ThePrimeagen/harpoon"
+  },
+
+  "folke/trouble.nvim",
+
+  -- To make a plugin not be loaded
+  {
+    "NvChad/nvim-colorizer.lua",
+    enabled = false
+  },
+
+  -- Uncomment if you want to re-enable which-key
+  -- {
+  --   "folke/which-key.nvim",
+  --   enabled = true,
+  -- },
+}
+
+return plugins
