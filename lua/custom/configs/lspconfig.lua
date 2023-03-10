3cf452845f853eadd24906f14d1ce73ce2a9be29
ayushmishra <mail81mitthu@gmail.com>
2023-03-10 18:58:55 +0530

    i am ded

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -10,16 +10,4 @@ for _, lsp in ipairs(servers) do
     on_attach = on_attach,
     capabilities = capabilities,
   }
-  if lsp == "rust_analyzer" then
-    lspconfig.rust_analyzer.setup {
-      on_attach = on_attach,
-      settings = {
-        ["rust-analyzer"] = {
-          check = {
-            extraArgs = { "-r" },
-          },
-        },
-      },
-    }
-  end
 end
