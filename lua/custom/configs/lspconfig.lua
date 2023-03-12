8a0e995577efebe4358d22a481c27791b3ff190d
ayushmishra <mail81mitthu@gmail.com>
2023-03-12 19:36:59 +0530

    finally got it working (use the custom ra's build)

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -15,8 +15,14 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
-lspconfig.pyright.setup {on_attach = on_attach, capabilities = capabilities}
+lspconfig.pyright.setup { on_attach = on_attach, capabilities = capabilities }
 lspconfig.rust_analyzer.setup {
-    on_attach = on_attach,
-    settings = {["rust-analyzer"] = {check = {extraArgs = {"-r"}}}}
+  on_attach = on_attach,
+  settings = { 
+    ["rust-analyzer"] = {
+      cargo = { 
+        extraArgs = { "-r" } 
+      } 
+    } 
+  },
 }
